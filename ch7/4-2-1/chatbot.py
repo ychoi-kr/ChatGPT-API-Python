b4b133f05cd6b37d0076842c487b078515e76b00
Yong Choi <sk8er.choi@gmail.com>
2024-02-21 14:07:38 +0900

    한국어 문서로 교체

--- ch7/4-2-1/chatbot.py
+++ ch7/4-2-1/chatbot.py
@@ -3,9 +3,9 @@ from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 
-loader = PyPDFLoader("digital_01_202107_keikaku.pdf")
+loader = PyPDFLoader("★ 서울특별시 스마트도시 및 정보화 기본계획(홈페이지 게시용).pdf")
 
 index = VectorstoreIndexCreator().from_loaders([loader])
-print("質問を入力してください")
+print("질문을 입력하세요")
 answer = index.query(input())
 print(answer)
