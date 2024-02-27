7b9ee713a5cceb97ebba33a06a9faf6e7c2f56f1
Yong Choi <sk8er.choi@gmail.com>
2024-02-27 17:09:08 +0900

    remove unused import

--- ch7/4-2-1/chatbot.py
+++ ch7/4-2-1/chatbot.py
@@ -1,6 +1,4 @@
 from langchain.document_loaders import PyPDFLoader
-from langchain.vectorstores import Chroma
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 
 loader = PyPDFLoader("★ 서울특별시 스마트도시 및 정보화 기본계획(홈페이지 게시용).pdf")
